6922874ea393506be9307a1ed087c575e8b07548
Argenio <argenio.tec@gmail.com>
2021-11-04 17:53:50 -0300

    Aula 03 - Exercício Aula 3 - Aula 04 - Exercício Aula 4

---
+++ Curso de Arduino - Flávio Guimarães/1 - Módulo 1 - Eletrônica básica (12 aulas)/03 - Aula 03 - Como Funciona a Eletrônica/sketch.ino
@@ -0,0 +1,40 @@
+/*
+  Curso de Arduino .NET - Flávio Guimarães
+  CANAL: Brincando com Ideias - https://www.youtube.com/c/BrincandocomIdeias
+  SITE: https://www.brincandocomideias.com/
+  Autor:  Argênio Silveira 
+  Link:   https://wokwi.com/arduino/projects/314356632320475714
+  MODULO: 1 - Módulo 1 - Eletrônica básica (12 aulas) 
+  SKETCH: 03 - Aula 03 - Como Funciona a Eletrônica
+*/
+/*
+Circuito aberto e circuito fechado.
+
+O circuito elétrico (Aberto) se caracteriza por uma interrupção nos fios de ligação do circuito
+que constitui o caminho a ser percorrido pela corrente elétrica.
+
+A corrente elétrica percorre do sentido pólo + para o pólo -,pelos fios de ligação, sem impedimento nenhum,
+pois a chave está fechada no Caso o Circuito esta (Fechado).
+
+Uma Fonte trabalha en desequilibrio por isso a corrente percorre do polo + pata o - !
+------------------
+Fenômenos da eletricidade: corrente, tensão e resistência.
+Corrente -  AMPER (A)
+Tensão -  VOLT (V)
+Resistência - OHM (Ω)
+Unidades de medida: Amper, Volt e Ohm.
+
+Exercício: Partes do circuito eletrico, nomes, uni
+A voltagem de um raio encontra-se entre 100 milhões e 1 bilhão de Volts.
+Sua corrente é da ordem de 30 mil Ampères, ou seja, a mesma utilizada por 30 mil lâmpadas de 100 W juntas.
+Em alguns raios, a corrente pode chegar a 300 mil Ampères!
+*/
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
